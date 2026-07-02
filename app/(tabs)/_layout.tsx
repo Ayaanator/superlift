3f5fb9640ed7409167c5ea4b2ccb682aa808d817
Ayaan Adrito <ayaan.adrito@gmail.com>
2026-07-01 23:12:12 -0400

    replace workout as index file instead (MAYBE THIS WILL WORK... PLEASE!!!! 🙏🙏🙏)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,7 +44,7 @@ export default function TabLayout() {
           }}
         />*/}
         <Tabs.Screen
-          name="workout"
+          name="index"
           options={{
             title: 'Workout',
             tabBarIcon: ({ color }) => (
